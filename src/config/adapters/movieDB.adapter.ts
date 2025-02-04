96c3a0d7361b28f46bb7342779f87f049929825c
andres <60414032+sandres132@users.noreply.github.com>
2025-02-03 21:03:17 -0600

    Variables de entorno agregadas

--- src/config/adapters/movieDB.adapter.ts
+++ src/config/adapters/movieDB.adapter.ts
@@ -1,10 +1,12 @@
+import { THE_MOVIE_DB_KEY } from "@env";
 import { AxiosAdapter } from "./http/axios.adapter";
 
 
 export const movieDBFetcher = new AxiosAdapter({
     baseUrl: 'https://api.themoviedb.org/3/movie',
     Params: {
-        api_key: '9f972e2a21d42372c7dff1dc69bcd80d',
+        // api_key: '9f972e2a21d42372c7dff1dc69bcd80d',
+        api_key: THE_MOVIE_DB_KEY ?? 'No key',
         language: 'es',
     }
-})
+});
